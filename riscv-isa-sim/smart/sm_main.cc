6c9fe54aea3d2a15e5e5e9b4c0b555056275db11
YoungLee1995 <liyang950115@gmail.com>
2025-10-10 11:24:41 +0800

    更新代码

--- riscv-isa-sim/smart/sm_main.cc
+++ riscv-isa-sim/smart/sm_main.cc
@@ -8,18 +8,37 @@
 #include "sm_dispatch.h"
 #include "sm_file.h"
 
-sm_main_t *g_sm_main;
+#include <vector>
+#include <filesystem>
 
-void sm_start(sim_t *sim)
+sm_main_t *sm_start(sim_t *sim)
 {
-    g_sm_main = new sm_main_t(sim);
     sm_log_info("smart extension start");
+    return new sm_main_t(sim);
+}
+
+void sm_main_t::tick(uint64_t ncycles)
+{
+    curr_cycles = ncycles;
+    for (auto p : Npus)
+        p->tick(ncycles);
+    Ddr->tick(ncycles);
 }
 
 sm_main_t::sm_main_t(sim_t *sim)
 {
     Sim = sim;
     Bus = &sim->bus;
+
+    Barrier = new sm_barrier_t();
+    Ddr = new sm_npu_ddr_t(sim);
+    for (int i = 0; i < SM_NPU_CORES; i++)
+    {
+        Npus[i] = new sm_npu_t(sim, this, i);
+        Rope[i] = new sm_ref_rope_t();
+    }
+
+    filesystem::path cwd=filesystem::current_path().parent_path();
 
     Dispatch = new sm_dispatch_t(sim);
     Bus->add_device((reg_t)SM_DISPATCH_BASE, Dispatch);
--- riscv-isa-sim/smart/smart.h
+++ riscv-isa-sim/smart/smart.h
@@ -10,23 +10,38 @@ class bus_t;
 
 class sm_dispatch_t;
 class sm_file_t;
+class sm_barrier_t;
 class sm_main_t;
+class sm_npu_t;
+class sm_npu_ddr_t;
+class sm_ref_rope_t;
+class sm_ref_softmax_t;
+class sm_ref_llm_vec_reduce_t;
+class sm_config;
 
-extern sm_main_t *g_sm_main;
-extern void sm_start(sim_t *sim);
+sm_main_t *sm_start(sim_t *sim);
 
 class sm_main_t
 {
     sim_t *Sim;
     bus_t *Bus;
-    sm_dispatch_t *Dispatch;
-    sm_file_t *File;
 
 public:
-    sm_npu_t *Npu[SM_NPU_CORES];
+    sm_npu_t *Npus[SM_NPU_CORES];
+    sm_npu_ddr_t *Ddr;
+    sm_barrier_t *Barrier;
+    sm_ref_rope_t *Rope[SM_NPU_CORES];
+    sm_dispatch_t *Dispatch;
+    sm_file_t *File;
+    sm_ref_softmax_t *Softmax[SM_NPU_CORES];
+    sm_ref_llm_vec_reduce_t *Vec_reduce[SM_NPU_CORES];
+    sm_config *Cfg;
+    uint64_t curr_cycles;
 
     sm_main_t(sim_t *sim);
-    int tick();
+    sm_barrier_t *get_barrier() { return Barrier; }
+    void tick(uint64_t cycles);
+    void set_global_params(sm_config *cfg);
 };
 
 class sm_npucore_exit_t
@@ -43,16 +58,14 @@ class sm_npucore_barrier_t
 {
 public:
     unsigned int Ecode;
-    sm_npucore_barrier_t(){};
-   
+    sm_npucore_barrier_t() {};
 };
 
 class sm_npucore_full_t
 {
 public:
     unsigned int Ecode;
-    sm_npucore_full_t(){};
+    sm_npucore_full_t() {};
 };
-
 
 #endif // _smart_h
